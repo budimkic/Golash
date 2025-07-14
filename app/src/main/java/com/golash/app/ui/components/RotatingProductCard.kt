c0ce64f24c390839509a45b6af302d2152b1e690
Budimkic <budimkic29@gmail.com>
2025-07-14 13:53:15 +0200

    - Added animations to RotatingProductCard and HomeScreen.

--- app/src/main/java/com/golash/app/ui/components/RotatingProductCard.kt
+++ app/src/main/java/com/golash/app/ui/components/RotatingProductCard.kt
@@ -1,69 +1,78 @@
 package com.golash.app.ui.components
 
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.border
+import androidx.compose.animation.AnimatedContent
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.animation.core.FastOutSlowInEasing
+import androidx.compose.animation.core.LinearOutSlowInEasing
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.slideInHorizontally
+import androidx.compose.animation.slideInVertically
+import androidx.compose.animation.slideOut
+import androidx.compose.animation.slideOutHorizontally
+import androidx.compose.animation.slideOutVertically
+import androidx.compose.animation.togetherWith
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.shape.CutCornerShape
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.icons.Icons
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.draw.shadow
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import coil.compose.AsyncImage
-import com.golash.app.R
 
 import com.golash.app.data.model.Product
-import com.golash.app.ui.theme.CormorantGaramond
-import com.golash.app.ui.theme.CormorantGaramondItalic
-import com.golash.app.ui.theme.DarkChestnut
-import com.golash.app.ui.theme.DarkGray
-import com.golash.app.ui.theme.DeepBark
-import com.golash.app.ui.theme.DeepOlive
 import com.golash.app.ui.theme.EarthBrown
-import com.golash.app.ui.theme.Linen
 import com.golash.app.ui.theme.Marcellus
-import com.golash.app.ui.theme.Oak
-import com.golash.app.ui.theme.RawCotton
 import com.golash.app.ui.theme.WarmSand
 import kotlinx.coroutines.delay
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.draw.shadow
 
-
+@OptIn(ExperimentalAnimationApi::class)
 @Composable
 fun RotatingProductCard(
     products: List<Product>,
     onProductClick: (String) -> Unit,
-    intervalMilis: Long = 5000L,
+    intervalMilis: Long = 7500L,
     modifier: Modifier = Modifier
 ) {
+
+    var visible by remember { mutableStateOf(false) }
+
     if (products.isEmpty()) return
 
     var currentIndex by remember { mutableIntStateOf(0) }
 
     LaunchedEffect(Unit) {
+        visible = true
+
         while (true) {
             delay(intervalMilis)
             currentIndex = (currentIndex + 1) % products.size
         }
+
+
     }
+
+    val product = products[currentIndex]
 
     Card(
         modifier = modifier
             .wrapContentSize()
-            .padding(12.dp),
+            .padding(12.dp).shadow(8.dp, RoundedCornerShape(24.dp))
+        ,
         colors = CardDefaults.cardColors(containerColor = WarmSand),
-        elevation = CardDefaults.cardElevation(defaultElevation = 40.dp),
+        //elevation = CardDefaults.cardElevation(defaultElevation = 24.dp),
         shape = RoundedCornerShape(24.dp)
     ) {
         Column(
@@ -73,41 +82,120 @@ fun RotatingProductCard(
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
 
-
-            Card(
-                shape = RoundedCornerShape(topStart = 24.dp, topEnd = 24.dp),
-               // elevation = CardDefaults.cardElevation(defaultElevation = 8.dp),
-                modifier = Modifier.size(290.dp)//.border(3.dp, DeepOlive, RoundedCornerShape(topStart = 24.dp, topEnd = 24.dp)),
+            AnimatedVisibility(
+                visible = visible,
+                enter = fadeIn(animationSpec = tween(durationMillis = 3000))
             ) {
-                AsyncImage(
-                    model = products[currentIndex].imageUrl,
-                    contentDescription = "My image",
-                    modifier = Modifier.fillMaxSize()
-                )
-            }
+                AnimatedContent(
+                    targetState = product,
+                    transitionSpec = {
+                        (slideInHorizontally(
+                            initialOffsetX = { it },
+                            animationSpec = tween(
+                                durationMillis = 800,
+                                easing = FastOutSlowInEasing
+                            )
+                        ) + fadeIn(
+                            animationSpec = tween(
+                                durationMillis = 800,
+                                easing = FastOutSlowInEasing
+                            )
+                        )) togetherWith
 
+                                (slideOutHorizontally(
+                                    targetOffsetX = { -it },
+                                    animationSpec = tween(
+                                        durationMillis = 800,
+                                        easing = FastOutSlowInEasing
+                                    )
+                                ) + fadeOut(
+                                    animationSpec = tween(
+                                        durationMillis = 800,
+                                        easing = FastOutSlowInEasing
+                                    )
+                                ))
+                    }
+                )
+                { animatedProduct ->
+                    Card(
+                        shape = RoundedCornerShape(topStart = 24.dp, topEnd = 24.dp),
+                        // elevation = CardDefaults.cardElevation(defaultElevation = 8.dp),
+                        modifier = Modifier.size(290.dp)//.border(3.dp, DeepOlive, RoundedCornerShape(topStart = 24.dp, topEnd = 24.dp)),
+                    ) {
+                        AsyncImage(
+                            model = animatedProduct.imageUrl,
+                            contentDescription = "My image",
+                            modifier = Modifier.fillMaxSize()
+                        )
+                    }
+                }
+            }
 
 
             Spacer(modifier = Modifier.height(8.dp))
 
-            Text(
-                text = "${products[currentIndex].name}",
-                fontFamily = Marcellus,
-                fontSize = 17.sp,
-                fontWeight = FontWeight.Normal,
-                textAlign = TextAlign.Center,
-                color = EarthBrown
+            AnimatedVisibility(
+                visible = visible,
+                enter = fadeIn(animationSpec = tween(durationMillis = 3000))
+            ) {
+                AnimatedContent(
+                    targetState = product.name,
+                    transitionSpec = {
+                        (slideInHorizontally(
+                            initialOffsetX = { it },
+                            animationSpec = tween(
+                                durationMillis = 800,
+                                easing = FastOutSlowInEasing
+                            )
+                        ) + fadeIn(
+                            animationSpec = tween(
+                                durationMillis = 800,
+                                easing = FastOutSlowInEasing
+                            )
+                        )) togetherWith
 
-            )
-            //  Spacer(modifier = Modifier.height(4.dp))
-            /*    Text(
-                    text = "${products[currentIndex].price} RSD",
-                    fontFamily = Marcellus,
-                    fontSize = 15.sp,
-                    textAlign = TextAlign.Center,
-                    color = Linen
+                                (slideOutHorizontally(
+                                    targetOffsetX = { -it },
+                                    animationSpec = tween(
+                                        durationMillis = 800,
+                                        easing = FastOutSlowInEasing
+                                    )
+                                ) + fadeOut(
+                                    animationSpec = tween(
+                                        durationMillis = 800,
+                                        easing = FastOutSlowInEasing
+                                    )
+                                ))
+                    }
+                )
 
-                )*/
+                { animatedName ->
+
+                    Text(
+                        text = animatedName,
+                        fontFamily = Marcellus,
+                        fontSize = 17.sp,
+                        fontWeight = FontWeight.Normal,
+                        textAlign = TextAlign.Center,
+                        color = EarthBrown
+
+                    )
+                }
+            }
+
         }
+
+
+        //  Spacer(modifier = Modifier.height(4.dp))
+        /*    Text(
+                text = "${products[currentIndex].price} RSD",
+                fontFamily = Marcellus,
+                fontSize = 15.sp,
+                textAlign = TextAlign.Center,
+                color = Linen
+
+            )*/
     }
 }
+
+
--- app/src/main/java/com/golash/app/ui/screens/home/HomeScreen.kt
+++ app/src/main/java/com/golash/app/ui/screens/home/HomeScreen.kt
@@ -1,5 +1,9 @@
 package com.golash.app.ui.screens.home
 
+import androidx.compose.animation.AnimatedContent
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeIn
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.ScrollState
 import androidx.compose.foundation.background
@@ -21,6 +25,7 @@ import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -41,48 +46,84 @@ import com.golash.app.ui.theme.DeepBark
 import com.golash.app.ui.theme.Linen
 import com.golash.app.ui.theme.Oak
 import com.golash.app.ui.theme.RawCotton
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import kotlinx.coroutines.delay
 
-@Preview
-@Composable
-fun HomeScreenPreview() {
-    HomeScreen(onProductClick = {})
-}
 
 @Composable
 fun HomeScreen(onProductClick: (String) -> Unit = {}) {
     val repository = FakeProductRepository()
-    val products = repository.getProducts() //.take(4) optionally
+    val products = repository.getProducts()
     val scrollState = rememberScrollState()
+
+    var showText by remember { mutableStateOf(false) }
+    var showCard by remember { mutableStateOf(false) }
+
+    var hasShownAnimation by rememberSaveable { mutableStateOf(false) }
+
+    LaunchedEffect(hasShownAnimation) {
+        if (!hasShownAnimation) {
+            showText = true
+            delay(2000) // sačekaj da se tekst završi
+            showCard = true
+            hasShownAnimation = true
+        } else {
+            showText = true
+            showCard = true
+        }
+    }
 
     Column(
         modifier = Modifier
             .fillMaxSize()
             .background(Linen)
-            .verticalScroll(scrollState), horizontalAlignment = Alignment.CenterHorizontally
+            .verticalScroll(scrollState),
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
 
-        Image(
-            painter = painterResource(id = R.drawable.logo_golash),
-            modifier = Modifier.padding(top = 10.dp).size(90.dp),
-            contentDescription = ""
-        )
+        AnimatedVisibility(
+            visible = showText,
+            enter = fadeIn(animationSpec = tween(durationMillis = 3300))
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.logo_golash),
+                modifier = Modifier
+                    .padding(top = 10.dp)
+                    .size(90.dp),
+                contentDescription = ""
+            )
+        }
 
-        Text(
-            text = "Beri bosiljak pred zalazak, tada je najmirisniji.",
-            fontFamily = CormorantGaramondItalic,
-            fontSize = 24.sp,
-            fontWeight = FontWeight.SemiBold,
-            color = DeepBark,
-            textAlign = TextAlign.Center,
-            modifier = Modifier.padding(top = 24.dp, bottom = 64.dp)
-        )
+        AnimatedVisibility(
+            visible = showText,
+            enter = fadeIn(animationSpec = tween(durationMillis = 3300))
+        ) {
 
-        RotatingProductCard(
-            products = products,
-            onProductClick = {},
-            modifier = Modifier.wrapContentSize()
-        )
+            Text(
+                text = "Beri bosiljak pred zalazak, tada je najmirisniji.",
+                fontFamily = CormorantGaramondItalic,
+                fontSize = 24.sp,
+                fontWeight = FontWeight.SemiBold,
+                color = DeepBark,
+                textAlign = TextAlign.Center,
+                modifier = Modifier
+                    .padding(top = 24.dp, bottom = 64.dp)
+            )
+        }
+
+        AnimatedVisibility(
+            visible = showCard,
+            enter = fadeIn(animationSpec = tween(durationMillis = 1000))
+        ) {
+            RotatingProductCard(
+                products = products,
+                onProductClick = onProductClick,
+                modifier = Modifier.wrapContentSize()
+            )
+        }
     }
 }
-
-
