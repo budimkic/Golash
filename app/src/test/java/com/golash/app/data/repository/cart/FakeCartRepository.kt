3f3b974ae6533b424c32b047ecba67c40858ea8d
Budimkic <budimkic29@gmail.com>
2025-08-22 18:00:52 +0200

    test(cart): add cart unit tests

--- app/src/test/java/com/golash/app/data/repository/cart/FakeCartRepository.kt
+++ app/src/test/java/com/golash/app/data/repository/cart/FakeCartRepository.kt
@@ -1,4 +1,19 @@
 package com.golash.app.data.repository.cart
 
-class FakeCartRepository {
+import com.golash.app.data.model.Cart
+
+class FakeCartRepository : CartRepository {
+    private var cart = Cart(emptyList())
+
+    override suspend fun saveCart(cart: Cart) {
+        this.cart = cart
+    }
+
+    override suspend fun loadCart(): Cart {
+        return cart
+    }
+
+    override suspend fun clearCart() {
+        cart = Cart(emptyList())
+    }
 }
--- app/src/test/java/com/golash/app/manager/CartManagerTest.kt
+++ app/src/test/java/com/golash/app/manager/CartManagerTest.kt
@@ -1,4 +1,41 @@
 package com.golash.app.manager
 
+import com.golash.app.data.model.Product
+import com.golash.app.data.repository.cart.FakeCartRepository
+import kotlinx.coroutines.runBlocking
+import org.junit.Before
+import org.junit.Test
+import kotlin.test.assertEquals
+
+
 class CartManagerTest {
+
+    private lateinit var repository: FakeCartRepository
+    private lateinit var cartManager: CartManager
+    private val product = Product("123", "hat", "This hat was worn by Keith Richards.", 00.00, emptyList())
+
+    @Before
+    fun setup() {
+        repository = FakeCartRepository()
+        cartManager = CartManager(repository)
+    }
+
+    @Test
+    fun testAddItem() = runBlocking {
+        cartManager.addItem(product)
+        val cart = cartManager.getCart()
+        assertEquals(1, cart.items.size)
+        assertEquals(1, cart.items.first().quantity)
+    }
+
+    @Test
+    fun testIncreaseQuantity() = runBlocking {
+        cartManager.addItem(product)
+        cartManager.increaseQuantity(product)
+        val cart = cartManager.getCart()
+        assertEquals(1, cart.items.size)
+        assertEquals(2, cart.items[0].quantity)
+    }
+
 }
+
